393f5b6cb7501d7e96ef41af9702b16c4b35d433
RationalGeographic <30840845+RationalGeographic@users.noreply.github.com>
2018-03-30 18:35:08 -0700

    popz
    no

--- src/Hooses.java
+++ src/Hooses.java
@@ -5,6 +5,8 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Hooses {
+	static NewHouse housy = new NewHouse();
+
 	public static void main(String[] args) {
 
 		Robot robo = new Robot();
@@ -39,47 +41,13 @@ public class Hooses {
 			JOptionPane.showMessageDialog(null, "large houses have to have flat roofs");
 		}
 		for (int i = 0; i < 10; i++) {
-			house(robo, hite, color, ruf);
+			housy.house(robo, hite, color, ruf);
+		hotline(robo);
 		}
 	}
-NewHouse housy = new NewHouse();
 
-	public static void house(Robot robo, int hite, Color color, String ruf) {
-		robo.setPenColor(color);
-		robo.move(hite);
-		if (hite == 500) {
-			flatroof(robo);
+	public static void hotline(Robot robo) {
 
-		}
-
-		else if (ruf.equals("flat")) {
-			flatroof(robo);
-		} else {
-			pointyroof(robo);
-		}
-
-		robo.move(hite);
-		robo.setAngle(90);
-		robo.setPenColor(Color.green);
-		robo.move(30);
-		robo.setAngle(360);
-
+		housy.flatroof(robo);
 	}
-
-	public static void flatroof(Robot robo) {
-
-		robo.turn(90);
-		robo.move(40);
-		robo.setAngle(180);
-
-	}
-
-	public static void pointyroof(Robot robo) {
-		robo.turn(45);
-		robo.move(30);
-		robo.turn(90);
-		robo.move(30);
-		robo.setAngle(180);
-	}
-
 }
